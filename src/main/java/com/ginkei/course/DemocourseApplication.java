0de580a949738b1ddeb77be01e0090b9cd6f9340
guina86 <guina86@gmail.com>
2021-03-20 08:20:49 -0300

    Project created

---
+++ src/main/java/com/ginkei/course/DemocourseApplication.java
@@ -0,0 +1,13 @@
+package com.ginkei.course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemocourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemocourseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ginkei/course/DemocourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ginkei.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemocourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
