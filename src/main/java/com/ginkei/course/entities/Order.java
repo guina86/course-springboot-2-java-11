78781f4248d989dea1bff2380844d3ecb743d0c6
guina86 <guina86@gmail.com>
2021-03-20 21:58:15 -0300

    Subtotal & Total methods

--- src/main/java/com/ginkei/course/entities/Order.java
+++ src/main/java/com/ginkei/course/entities/Order.java
@@ -31,7 +31,7 @@ public class Order implements Serializable {
 
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
 	private Instant moment;
-	
+
 	private Integer orderStatus;
 
 	@ManyToOne
@@ -40,10 +40,10 @@ public class Order implements Serializable {
 
 	@OneToMany(mappedBy = "id.order")
 	private Set<OrderItem> items = new HashSet<>();
-	
+
 	@OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
 	private Payment payment;
-	
+
 	public Order() {
 	}
 
@@ -70,14 +70,14 @@ public class Order implements Serializable {
 	public void setMoment(Instant moment) {
 		this.moment = moment;
 	}
-	
+
 	public OrderStatus getOrderStatus() {
 		return OrderStatus.valueOf(orderStatus);
 	}
 
 	public void setOrderStatus(OrderStatus orderStatus) {
-		if(orderStatus != null) {
-			this.orderStatus = orderStatus.getCode();	
+		if (orderStatus != null) {
+			this.orderStatus = orderStatus.getCode();
 		}
 	}
 
@@ -88,7 +88,7 @@ public class Order implements Serializable {
 	public void setClient(User client) {
 		this.client = client;
 	}
-	
+
 	public Payment getPayment() {
 		return payment;
 	}
@@ -97,8 +97,14 @@ public class Order implements Serializable {
 		this.payment = payment;
 	}
 
-	public Set<OrderItem> getItems(){
+	public Set<OrderItem> getItems() {
 		return items;
+	}
+
+	public Double getTotal() {
+		return items.stream()
+				.mapToDouble(item -> item.getSubTotal())
+				.reduce(0,(total, itemValue) -> total + itemValue);
 	}
 
 	@Override
--- src/main/java/com/ginkei/course/entities/enums/OrderStatus.java
+++ src/main/java/com/ginkei/course/entities/enums/OrderStatus.java
@@ -1,5 +1,7 @@
 package com.ginkei.course.entities.enums;
 
+import java.util.Arrays;
+
 public enum OrderStatus {
 	WAITING_PAYMENT(1),
 	PAID(2),
@@ -18,12 +20,10 @@ public enum OrderStatus {
 	}
 	
 	public static OrderStatus valueOf(int code) {
-		for(var value: OrderStatus.values()) {
-			if(value.getCode() == code) {
-				return value;
-			}
-		}
-		throw new IllegalArgumentException("Invalid OrderStatus code");
+		return Arrays.stream(OrderStatus.values())
+				.filter(status -> status.getCode() == code)
+				.findAny()
+				.orElseThrow(() -> new IllegalArgumentException("Invalid OrderStatus code"));
 	}
 	
 }
